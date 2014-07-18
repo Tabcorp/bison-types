831248acc16d624ac494ef88e787091665b6b029
nguyenchr <nguyenchr@tabcorp.com.au>
2014-07-18 18:05:36 +1000

    fix issue when loading from javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require("coffee-script");
+require("coffee-script/register");
 exports.Reader       = require("./reader");
 exports.Writer       = require("./writer");
 exports.preCompile   = require("./preCompile");
